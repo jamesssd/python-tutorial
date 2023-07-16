4b1bae955eace50a6b9fb92ddb4472f4cd717c61
James Dizon <dizonjerlin@gmail.com>
2023-07-15 17:55:58 -0700

    Completed Variables and started String

---
+++ Strings/app.py
@@ -0,0 +1,2 @@
+phrase = "James Dizon"
+print(phrase + " is cool")
--- Variables/app.py
+++ Variables/app.py
@@ -1,5 +1,6 @@
 character_name = "James"
 character_age = "29"
+
 print("Hello, my name is " + character_name)
 print("I'm " + character_age + " years old!")
 print("This is my first time studying Python")
