c4378a141aeece684444d6bfe92626efc573ca60
James Dizon <dizonjerlin@gmail.com>
2023-07-24 00:39:14 -0700

    Completed FizzBuzz ex

--- Day 5-For Loops, Range and Code Blocks/day-5-start.py
+++ Day 5-For Loops, Range and Code Blocks/day-5-start.py
@@ -1,5 +1,9 @@
-fruits = ["Apple", "Peach", "Pear"]
+# fruits = ["Apple", "Peach", "Pear"]
 
-for fruit in fruits :
-    print(fruit)
-    print(fruit + " Pie")
+# for fruit in fruits :
+#     print(fruit)
+#     print(fruit + " Pie")
+total_sum = 0
+for number in range (2, 101, 2) :
+    total_sum += number
+print(total_sum)
