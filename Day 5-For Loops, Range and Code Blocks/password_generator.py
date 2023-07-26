81a22a4895051ec15ff15d172806d4f949d87853
James Dizon <dizonjerlin@gmail.com>
2023-07-25 22:16:38 -0700

    Updated Code to show computer generated pass

--- Day 5-For Loops, Range and Code Blocks/password_generator.py
+++ Day 5-For Loops, Range and Code Blocks/password_generator.py
@@ -5,9 +5,9 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# nr_letters= int(input("How many letters would you like in your password?\n")) 
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
@@ -37,15 +37,43 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 #shuffle 
+
+#Hard Level Answer
+# password_list = []
+
+# for char in range(1, nr_letters + 1):
+#   password_list.append(random.choice(letters))
+
+# for char in range(1, nr_symbols + 1):
+#   password_list += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password_list += random.choice(numbers)
+
+# print(password_list)
+# random.shuffle(password_list)
+# print(password_list)
+
+# password = ""
+# for char in password_list:
+#   password += char
+
+# print(f"Your password is: {password}")
+
+#############
+#Program Generated Password
+random_num = random.randint(1,10)
+random_sym = random.randint(1,5)
+random_char = random.randint(1,10)
 password_list = []
 
-for char in range(1, nr_letters +1):
+for char in range(1, random_char):
     password_list.append(random.choice(letters))
 
-for sym in range(1, nr_symbols + 1) :
+for sym in range(1, random_sym) :
     password_list.append(random.choice(symbols))
     
-for num in range(1, nr_numbers +1):
+for num in range(1, random_sym):
     password_list.append(random.choice(numbers))
     
 print(password_list)
