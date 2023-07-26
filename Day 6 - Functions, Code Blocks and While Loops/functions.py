4e0f660cc7303529b9b077d9fba3aa4d89fc5f9c
James Dizon <dizonjerlin@gmail.com>
2023-07-26 08:36:26 -0700

    Completed Hurdle Three

---
+++ Day 6 - Functions, Code Blocks and While Loops/functions.py
@@ -0,0 +1,61 @@
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+# def my_function():
+#     print("This is a function")
+#     print("Hello")
+    
+# my_function()
+
+#Reeborg's World Hurdle Challange using loop
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_left
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# for x in range(6):
+#   jump()
+
+#************Hurdle Three***************#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_left
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+# while not at_goal():
+#     if wall_in_front() is True:
+#         turn_left()
+#         move()
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()
+#     elif front_is_clear() is True:
+#         move()
+#     else :
+#         jump()
