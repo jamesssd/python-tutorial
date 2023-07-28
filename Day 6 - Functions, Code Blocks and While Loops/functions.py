2d5d77d28cc430ec1d3253136c08117380c21b65
James Dizon <dizonjerlin@gmail.com>
2023-07-28 08:07:13 -0700

    Completed Hurdle Three

--- Day 6 - Functions, Code Blocks and While Loops/functions.py
+++ Day 6 - Functions, Code Blocks and While Loops/functions.py
@@ -59,3 +59,25 @@
 #         move()
 #     else :
 #         jump()
+
+#********Hurdle Three Answer******#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else :
+#         move()
