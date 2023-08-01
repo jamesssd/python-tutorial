b3b5aa36acbee594c661d4f3c2ecca3849539297
James Dizon <dizonjerlin@gmail.com>
2023-08-01 07:13:40 -0700

    Started Day 8 Functions with Input

---
+++ Day 8 - Functions With Input/start.py
@@ -0,0 +1,12 @@
+# def greet():
+#     print("hello")
+#     print("what up")
+#     print("bye")
+          
+# greet()
+
+def greet_name(name):
+    print(f"Hello {name}")
+    print(f"You ate all the macrons {name}")
+    
+greet_name("Cortny")
